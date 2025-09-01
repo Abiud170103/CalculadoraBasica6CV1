31073e8a042928b7f2f23dae1757d5e2a124d239
albertogzga <albertognzga@gmail.com>
2025-09-01 15:43:17 -0600

    Operacion suma

--- Calculadora.py
+++ Calculadora.py
@@ -4,3 +4,16 @@ num1 = float(input("Ingresa el primer numero: "))
 num2 = float(input("Ingresa el segundo numero: "))
 
 print("Operaciones ")
+
+print("1. Suma")
+print("2. Resta")
+print("3. Multiplicacion")
+print("4. Division")
+
+def suma(a,b):
+    return a + b
+
+opcion = input("Selecciona una operacion:")
+match opcion:
+    case "1":
+        print(suma(num1,num2))
